23ea2c8186e7cf868851cafac467c877b5e8479c
Bavi2005 <bavithrran@gmail.com>
2024-09-28 18:44:50 +0800

    Create studenthome_sidebar.php

---
+++ studenthome_sidebar.php
@@ -0,0 +1,16 @@
+<header class="header">
+    <a href="studenthome.php">Student Dashboard</a>
+    <div class="logout">
+        <a href="logout.php" class="btn btn-primary">Logout</a>
+    </div>
+</header>
+
+<aside>
+    <ul>
+        <li><a href="course_student.php">My Courses</a></li>
+        <li><a href="timetable.php">Timetable</a></li>
+        <li><a href="fees.php">Fees</a></li>
+        <li><a href="health.php">Health Management</a></li>
+        <li><a href="event_application.php">Event Hosting Application</a></li> <!-- Link to event application page -->
+    </ul>
+</aside>
