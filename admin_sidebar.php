bee524413b2e6eff150642aed23ba08318465696
Jason-0504 <jasgu02@gmail.com>
2024-09-28 18:17:59 +0800

    Create admin_sidebar.php

---
+++ admin_sidebar.php
@@ -0,0 +1,52 @@
+<header class="header">
+
+    <a href="adminhome.php">Admin Dashboard</a>
+
+    <dif class="logout">
+        <a href="logout.php" class="btn btn-primary">Logout</a>
+    </dif>
+</header>
+
+    <aside>
+
+    <ul>
+    .    
+    <li>
+        <a href="add_student.php">Add Student</a>
+    </li>
+    
+    <li>
+        <a href="Students.php">Students</a>
+    </li>
+
+    <li>
+        <a href="attendance.php">Attendance</a>
+    </li>
+
+    <li>
+        <a href="course_admin.php">Courses</a>
+    </li>
+
+    <li>
+        <a href="finance.php">Finance</a>
+    </li>
+
+    <li>
+        <a href="event_admin.php">Event Application</a>
+    </li>
+
+    <li>
+        <a href="health_admin.php">Health and Welness</a>
+    </li>
+
+    <li>
+        <a href="contact_us.php">Contact Us Form</a>
+
+    </li>
+
+
+
+    </ul>
+
+
+    </aside>
